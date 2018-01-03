0d24b47c827ab4afc44a23b45db178e240c379c6
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-03 16:52:24 -0700

    Add authentication component

---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Contacts extends Component {
+  render() {
+    return (
+      <h1>Contacts Page</h1>
+    )
+  }
+}
+
+export default Contacts;
+
---
+++ src/components/users/Users.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Users extends Component {
+  render() {
+    return (
+      <h1>Users Page</h1>
+    )
+  }
+}
+
+export default Users;
+

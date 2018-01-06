701551ddc98492d14100c51a9df3b28b459e31cf
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-06 15:38:10 -0700

    spike for auth

--- src/actions/Requests.js
+++ src/actions/Requests.js
@@ -1,11 +1,3 @@
-const providerLink = "https://www.linkedin.com/oauth/v2/authorization"
-
-function oauthRequest(){
-
-  fetch(providerLink)
-      .then(response => response.json())
-
-}
 
 
-export {oauthRequest}
+

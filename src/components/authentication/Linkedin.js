abd70f4ab11f9311dbc7b581af43784d30339c99
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-06 16:50:31 -0700

    clean up whitespace

--- src/components/authentication/Linkedin.js
+++ src/components/authentication/Linkedin.js
@@ -8,11 +8,9 @@ const requests = require('../../actions/Requests')
 class LoginWithLinkedin extends Component {
   render() {
     return (
-
   <a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=7870b7rz0apmts&redirect_uri=http://localhost:3000/auth/linkedin/callback&state=something">
     Sign in with Linkedin
   </a>
-
     )
   }
 }

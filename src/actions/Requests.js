dbd1fc490625060288f545ada7e47dc6a44a5240
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-06 21:29:19 -0700

    Add fetch for users

--- src/actions/Requests.js
+++ src/actions/Requests.js
@@ -1,3 +1,21 @@
 
 
+function fetchCurrentUser () {
 
+  fetch('http://localhost:3000/api/v1/users', {
+    mode: 'cors',
+    headers: {'Allow-Access-Control-Origin':'*'}
+  })
+  .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    // Now data is in a format we are more used to i.e. {"posts": [{"title": "Fetch Refresher", "author": "Katelyn Kasperowicz"},..]}
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+
+
+export {fetchCurrentUser}
--- src/components/authentication/Linkedin.js
+++ src/components/authentication/Linkedin.js
@@ -8,9 +8,12 @@ const requests = require('../../actions/Requests')
 class LoginWithLinkedin extends Component {
   render() {
     return (
-  <a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=7870b7rz0apmts&redirect_uri=http://localhost:3000/auth/linkedin/callback&state=something">
+      <a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=7870b7rz0apmts&redirect_uri=http://localhost:3000/auth/linkedin/callback&state=something"
+        onClick={console.log(requests.fetchCurrentUser())}
+      >
     Sign in with Linkedin
   </a>
+
     )
   }
 }

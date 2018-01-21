9f3b2ad16b25accc2fe8984328b8c6bf1fc35d0f
Daniel Phillips <danieledward.phillips@gmail.com>
2018-01-21 15:13:38 -0700

    Adds sized pic and welcome message to Home

--- src/components/authentication/Linkedin.js
+++ src/components/authentication/Linkedin.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Linkedin.css'
 const requests = require('../../actions/Requests')
 const user = require('../../actions/User')
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 
 
@@ -7,7 +8,10 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>This is your Home Page</h1>
+        <h1>Welcome to TechHustle.io</h1>
+        <section >
+          <img src="https://www.jobscan.co/blog/wp-content/uploads/2014/12/networking.jpg" className="background-image"/>
+        </section>
       </div>
     )
   }
